20d48e352c3294dd08f32d5978ff87700877196d
Si-hub <zweto.dayile@gmail.com>
2021-09-13 15:46:14 +0200

    second commit with index created on sqlite browser

--- src/main/java/Schools_Database/Schools.java
+++ src/main/java/Schools_Database/Schools.java
@@ -14,46 +14,46 @@ public class Schools{
          // create a database connection
          connection = DriverManager.getConnection("jdbc:sqlite:localSchools.db");
          Statement statement = connection.createStatement();
-         String sql = "insert into employees (" +
-                 "employees_id, first_name,last_name,company," +
-                 " email, phone_number,position) VALUES(?,?,?,?,?,?,?)";
+         String sql = "insert into employees (" + "employees_id, first_name," +
+                 " email, phone_number,position) VALUES(?,?,?,?,?)";
 
          statement.setQueryTimeout(30);  // set timeout to 30 sec.
 
-         statement.executeUpdate("DROP TABLE IF EXISTS employees");
+
          statement.executeUpdate("CREATE TABLE IF NOT EXISTS employees(" +
                  "employees_id integer PRIMARY KEY," +
-                 "first_name string,last_name string," + "company string," +
+                 "first_name string,"  +
                  "email string," +
                  "phone_number string," +
                  "position string)");
 
          ResultSet rs = statement.executeQuery("select * from employees");
+          PreparedStatement pstmt = connection.prepareStatement(sql);
 
          // Insert 10 000 records in the database
          int count = 0;
-         while(count < 10){
+         while(count < 10000){
              //creating faker object
             Faker faker = new Faker();
 
 
           String firstName = faker.name().firstName(); // "Sim"
-          String lastName= faker.name().lastName() ; // Dayile
-          String company = faker.company().name();// Codex
           String email = faker.internet().emailAddress(); // "sim@gmail.com"
           String phoneNumber = faker.phoneNumber().cellPhone(); // "0868353479"
           String position = faker.job().position();// programmer
 
-            PreparedStatement pstmt = connection.prepareStatement(sql);
-
             pstmt.setString(2, firstName);
-            pstmt.setString(3, lastName);
-            pstmt.setString(4, company);
-            pstmt.setString(5, email);
-            pstmt.setString(6, phoneNumber);
-            pstmt.setString(7, position);
+            pstmt.setString(3, email);
+            pstmt.setString(4, phoneNumber);
+            pstmt.setString(5, position);
 
             count++;
+
+             // read the result set
+            /* System.out.println("first_name = " + firstName + " | " +
+             "email = " + email + " | " +
+             "phone_number = " + phoneNumber + " | " +
+             "position = " + position);*/
 
             pstmt.executeUpdate();
          }
